b903777443f41bf0567a612687eb627434acffb8
Gabriel Warkentin <gabriel.warkentin@gmail.com>
2022-02-27 14:28:20 -0800

    placeholder for category nav

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import MainNav from './components/MainNav.vue'
+import CategoryNav from './components/CategoryNav.vue'
 
 </script>
 <template>
   <MainNav />
+  <CategoryNav />
   <div class="container">
-            <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
