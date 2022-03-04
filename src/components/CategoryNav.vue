07b05350f15c4718993d8c5513113afa3a88ce46
Gabriel Warkentin <gabriel.warkentin@gmail.com>
2022-03-04 00:16:51 -0800

    navbar note

--- src/components/CategoryNav.vue
+++ src/components/CategoryNav.vue
@@ -1,6 +1,13 @@
 <script>
 </script>
 
+<!-- Need to implement
+
+    Copy some general layout from Bootstrap documentation and MainNav 
+    Need to start thinking how the categories and subcategories should look
+    And how they should be designed on the navbar
+
+-->
 <template>
 </template>
 
