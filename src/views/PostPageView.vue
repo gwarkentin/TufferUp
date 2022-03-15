0f2752ad727c9ae88adcc50c1915abd3794f8b1a
Gabriel Warkentin <gabriel.warkentin@gmail.com>
2022-03-15 15:27:06 -0700

    detail view updates properly on load

--- src/views/PostPageView.vue
+++ src/views/PostPageView.vue
@@ -8,19 +8,22 @@ export default {
   data() {
     return {
       form: {
-        title: "Item Title",
-        description: "Item Description",
-        category: "Item Category",
-        condition: "Item Condition",
-        price: 999999.99,
+        title: "",
+        description: "",
+        category: "",
+        condition: "",
+        price: 0.0,
         discountable: false,
-        imgs: ["/src/assets/book.jpg"]
+        imgs: []
       },
       ogdata: true
     }
   },
   components: {
     PostDetails
+  },
+  mounted() {
+    this.tryAjax()
   },
   methods: {
 
@@ -29,7 +32,6 @@ export default {
      look at vue lifecycle hooks. We should calling tryAjax() from in Created(), but maybe check again
      later if something is going wrong
      */
-    
     tryAjax() {
       const id = (this.$route.params.id ? this.$route.params.id : "");
       axios.get('http://localhost:3001/api/post/' + id)
@@ -44,21 +46,19 @@ export default {
       .then(function () {
         // always executed
       });  
-    }
+    },
   }
 }
 </script>
 
 
 <template>
+  <p><strong>{{ $route.params.id }}</strong></p>
   <div class="row align-items-start">
     <!-- this button should go away, the page should immediate try to fetch the data, this is just for example -->
-    <button type="button" :class="[ogdata ? 'btn-primary' : 'btn-secondary']" class="btn" @click="tryAjax">Replace with Ajax Data</button>
     <div class="col-sm">
       <PostDetails v-bind="form" />
     </div>
   </div>
-  <p><strong>{{ $route.params.id }}</strong></p>
-  <p>id Param of /post/:id, try going to /post/1234 for example</p>
 </template>
 
