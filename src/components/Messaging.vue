6f1d3c79ed3e49de4a6c45ca2d314f2989af85af
EddySupra <Eduardo23@csu.fullerton.edu>
2022-04-29 20:13:14 -0700

    UPDATED chat box UI

--- src/components/Messaging.vue
+++ src/components/Messaging.vue
@@ -1,29 +1,29 @@
 <script>
-import { useUser } from '@/stores/user'
-import { useMessaging } from '@/stores/messaging'
+import { useUser } from "@/stores/user";
+import { useMessaging } from "@/stores/messaging";
 
 export default {
-  inject: ['$firebasemessaging'],
+  inject: ["$firebasemessaging"],
   setup() {
-    const userStore = useUser()
-    const messagingStore = useMessaging()
+    const userStore = useUser();
+    const messagingStore = useMessaging();
     return {
       userStore,
       messagingStore,
-    }
+    };
   },
   data() {
     return {
-      msg: '',
+      msg: "",
       thread: null,
-      error: ''
-    }
+      error: "",
+    };
   },
   props: {
-    receiverId: String
+    receiverId: String,
   },
   mounted() {
-    console.log('Firebase cloud messaging object', this.$firebasemessaging)
+    console.log("Firebase cloud messaging object", this.$firebasemessaging);
   },
   computed: {
     haserror() {
@@ -32,50 +32,112 @@ export default {
   },
   methods: {
     sendMessage() {
-      const msgform =  {
-        thread_id: this.messagingStore.msg_thread ? this.messagingStore.msg_thread._id : null,
+      const msgform = {
+        thread_id: this.messagingStore.msg_thread
+          ? this.messagingStore.msg_thread._id
+          : null,
         subscribers: [this.userStore.user.user],
         msg: {
           sender: this.userStore.user.user,
           text: this.msg,
-        }
-      }
+        },
+      };
       if (this.receiverId) {
-        msgform.subscribers.push(this.receiverId)
+        msgform.subscribers.push(this.receiverId);
       }
       this.messagingStore.sendMessage(msgform);
     },
-  }
-}
+  },
+};
 </script>
 
 <template>
-  <div class="row container-fluid position-fixed bottom-0">
-    <div class="col-md-3 offset-md-7 border bg-light bg-gradient" >
-      <div v-show="haserror" class="alert alert-danger" role="alert">
-        {{ error }}
-      </div>
-      <button type="button" class="btn-close" aria-label="Close"
-         data-bs-toggle="collapse" data-bs-target="#collapseMessage" aria-expanded="false" aria-controls="collapseMessage">
-      </button>
-      <div id="collapseMessage" class="collapse">
-        <div class="overflow-auto container-fluid">  <!-- scroll container, idk if we want ul/li !-->
-          <div class="row">
-            <div class="col-6 col-4">
-              <template v-if="messagingStore.msg_thread">
-                <div v-for="msg in messagingStore.msg_thread.msgs" :key="msg" class="bg-primary border">
-                  <span>{{ msg.text }}</span>
-                </div>
-              </template>
+  <div class="row position-fixed bottom-0 col-md-3 offset-md-7 chatbox mt-5">
+    <div v-show="haserror" class="alert alert-danger" role="alert">
+      {{ error }}
+    </div>
+    <button
+      type="button"
+      class="adiv"
+      aria-label="Close"
+      data-bs-toggle="collapse"
+      data-bs-target="#collapseMessage"
+      aria-expanded="false"
+      aria-controls="collapseMessage"
+    >Live Chat</button>
+    <div id="collapseMessage" class="collapse">
+      <div class="overflow-auto container-fluid">
+        <!-- scroll container, idk if we want ul/li !-->
+        <div class="col-6 col-4">
+          <template v-if="messagingStore.msg_thread">
+            <div
+              v-for="msg in messagingStore.msg_thread.msgs"
+              :key="msg"
+              class="chat ml-2 p-3"
+            >
+              <span>{{ msg.text }}</span>
             </div>
-          </div>
+          </template>
         </div>
-        <form @submit.prevent="sendMessage">
-          <input v-model="msg" type="text" />
-          <button type="submit">Send</button>
-        </form>
       </div>
+      <form @submit.prevent="sendMessage">
+        <input
+          v-model="msg"
+          type="text"
+          class="send"
+          placeholder="Send Message..."
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   </div>
 </template>
 
+<style>
+.chatbox {
+  background: #3e3e3e;
+  width: 300px;
+  border: none;
+  border-top-right-radius: 15px;
+  border-top-left-radius: 15px;
+}
+
+.adiv {
+  background: #cb5a04;
+  vertical-align: auto;
+  width: 300px;
+  border-radius: 15px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+  font-size: 12px;
+  height: 40px;
+  color: #ffffff;
+  text-align: center;
+}
+
+.chat {
+  border: none;
+  background: #0095ff;
+  font-size: 12px;
+  border-radius: 20px;
+  background-size: 5px;
+  overflow: auto;
+  row-gap: 100px;
+  align-content: flex-end;
+}
+
+.response {
+  border: none;
+  background: #40ff00;
+  font-size: 12px;
+  border-radius: 20px;
+  background-size: 5px;
+  overflow: auto;
+  row-gap: 100px;
+}
+
+.send {
+  border-radius: 15px;
+  background: #ffffff;
+}
+</style>
